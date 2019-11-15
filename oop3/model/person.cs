76277e7a0ea6a99ed3da4816d7b514500d703dff
Rand Farhan <rand.farhan@gmail.com>
2019-11-15 15:26:23 +0100

    Add project files.

---
+++ oop3/model/person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop3.NewFolder
+{
+    class person
+    {
+       readonly int id; 
+        string name;
+        string address;
+        string phone;
+        double salary;
+        const double tax = 0.1; 
+
+        public double getNetSalary()
+
+        {
+
+            double NetSal = salary - (salary * tax);
+            return NetSal; 
+
+        }
+
+        public string GetPerson()
+
+        {
+
+            string allData = "ID: " + id + "\n";
+            allData += "Name: " + name + "\n";
+            allData += "Address: " + address + "\n";
+            allData += "Phone: " + phone + "\n";
+            allData += "Salary: " + salary + "\n;";
+            allData += "Net Salary: " + getNetSalary ()+ "\n";
+
+            return allData; 
+
+        }
+
+        public void printPersonData()
+
+        {
+
+            Console.WriteLine(GetPerson());
+        }
+
+
+
+        
+    }
+}
